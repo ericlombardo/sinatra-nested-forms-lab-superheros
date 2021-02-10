5ac3d703bc3896008ba09b58f6abffa2fd75ba44
Eric <eric.m.lombardo@gmail.com>
2021-02-09 20:39:31 -0500

    Build forms and view files for hero model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+    
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :index
+    end
+
+    post '/teams' do
+        @team = params[:team]
+        @members = params[:team][:members]
+
+        erb :teams
+    end
 
 end
